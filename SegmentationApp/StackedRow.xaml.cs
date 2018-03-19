d5c29a5ea58bcf544cac7b9533b500e2eed6db54
rvnlvrz <rvnlvrz@gmail.com>
2018-03-19 19:46:07 +0800

    Add zooming functionality

--- SegmentationApp/StackedRow.xaml.cs
+++ SegmentationApp/StackedRow.xaml.cs
@@ -6,30 +6,13 @@ namespace SegmentationApp
 {
     /// <inheritdoc cref="UserControl" />
     /// <summary>
-    /// Interaction logic for StackedRow.xaml
+    ///     Interaction logic for StackedRow.xaml
     /// </summary>
     public partial class StackedRow
     {
-        /// <summary>
-        /// Sets or Gets collection of series that are bound to the <see cref="StackedRow"/> control.
-        /// </summary>
-        public SeriesCollection SeriesCollection { get; set; }
-
-        /// <summary>
-        /// Contains the labels that are used for the Y-Axis of the graph
-        /// </summary>
-        public string[] Labels { get; set; }
-
-
-        /// <summary>
-        /// Sets or Gets the format of the string label for the graph separators.
-        /// </summary>
-        public Func<double, string> Formatter { get; set; }
-
-
         /// <inheritdoc />
         /// <summary>
-        /// Creates a new instance of the <see cref="StackedRow"/> class.
+        ///     Creates a new instance of the <see cref="StackedRow" /> class.
         /// </summary>
         public StackedRow()
         {
@@ -38,7 +21,23 @@ namespace SegmentationApp
         }
 
         /// <summary>
-        /// Sets the datacontext of the Stacked Row control to this class.
+        ///     Sets or Gets collection of series that are bound to the <see cref="StackedRow" /> control.
+        /// </summary>
+        public SeriesCollection SeriesCollection { get; set; }
+
+        /// <summary>
+        ///     Contains the labels that are used for the Y-Axis of the graph
+        /// </summary>
+        public string[] Labels { get; set; }
+
+
+        /// <summary>
+        ///     Sets or Gets the format of the string label for the graph separators.
+        /// </summary>
+        public Func<double, string> Formatter { get; set; }
+
+        /// <summary>
+        ///     Sets the datacontext of the Stacked Row control to this class.
         /// </summary>
         public void BindContext()
         {
